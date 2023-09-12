0ea91874ae07d17712a91a62316e97a8e58214e6
hamelln <vusdus91@gmail.com>
2023-09-12 18:50:38 +0900

    fix: 예제 코드 변경

--- chapter-2/7.여러 타입.ts
+++ chapter-2/7.여러 타입.ts
@@ -35,12 +35,14 @@ const flowerLatinName = (flower: Flower) => {
 
 //@ 에러를 던지는 코드와 무한 실행 코드도 비슷한 원리다.
 //? 절대로 뭔가를 return할 일이 없음. 그러므로 반환 타입이 never
-function throwError(errorMsg: string): never {
-  throw new Error(errorMsg);
+function timeout(ms: number): Promise<never> {
+  return new Promise((_, reject) =>
+    setTimeout(() => reject(new Error("fetching 허용 시간 초과")), ms)
+  );
 }
 
 function keepProcessing(): never {
-  while (true) {}
+  while (true) {} // return에 다다를 수 없다.
 }
 
 //# unknown: "무슨 타입인지 아직 모른다."
